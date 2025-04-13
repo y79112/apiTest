09335cc457c5b04d21726c132a4a4e48151cfa5c
凌寒 <y7112@qq.com>
2025-04-13 10:15:51 +0800

    增加登录对象

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,16 @@ author: y79112
 #测试用例执行配置
 
 import pytest
+from libs.login import Login
 
 @pytest.fixture(scope='session',autouse=True)
 def start_end_test():
     print('\n开始测试...')
     yield
     print('测试结束。')
+
+
+@pytest.fixture(scope='module')
+def login_init(request):
+    login_obj = Login()
+    yield login_obj
